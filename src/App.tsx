fe23cc3acd5df73296b1f34ae524b14ab77e89ba
dontdoithere <seeema34l@gmail.com>
2023-04-25 17:29:22 -0400

    Develodep css code for containers and added 2 pictures

--- src/App.tsx
+++ src/App.tsx
@@ -23,19 +23,21 @@ const App: React.FC = () => {
         <p>Hello, I am Simon a passionate developer with experience in various programming languages and frameworks. I am always eager to learn new technologies and take on new challenges. Welcome to my portfolio website!</p>
     </section>}
     {
-      <section id="projects">/
-      <h2>My Projects</h2>
-      <div>
-          <article>
-              <h3>Project #1</h3>
-              <p>Description of Project #1 goes here. You can mention the details, technologies used, and any other relevant information.</p>
-          </article>
-          <article >
-              <h3>Project #2</h3>
-              <p>Description of Project #2 goes here. You can mention the details, technologies used, and any other relevant information.</p>
-          </article>
+      <section id="projects">
+      <h2 id="h-projects">My Projects</h2>
+      <div className="projects-container">
+        <article className="project-box">
+          <h3>Art object #1-?-</h3>
+          <img src="./pictures/first.png" alt="Project 1 Image" />
+          <p>Description of Project #1 goes here. You can mention the details, technologies used, and any other relevant information.</p>
+        </article>
+        <article className="project-box">
+          <h3>Project #2</h3>
+          <img src="./pictures/second-picture.png" alt="Project 2 Image" />
+          <p>Description of Project #2 goes here. You can mention the details, technologies used, and any other relevant information.</p>
+        </article>
       </div>
-  </section>
+    </section>
     }
     </>
   );
